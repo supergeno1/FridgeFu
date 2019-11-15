bb22f5ee772861661b5ffe0849cdb74b8699176f
Renuka Nannapaneni <renuka@wirelessprv-10-194-248-17.near.illinois.edu>
2019-11-15 11:40:09 -0600

    initial activities

---
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/GroceryLanding.kt
@@ -0,0 +1,12 @@
+package com.location.android.fridgefu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GroceryLanding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_grocery_landing)
+    }
+}
