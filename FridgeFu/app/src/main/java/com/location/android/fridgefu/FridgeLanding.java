8e777b758bde97e578d591320d1fd7d9a56cf3d2
Navam <uawasth2@illinois.edu>
2019-11-15 13:17:11 -0600

    fridge outline

---
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/FridgeLanding.java
@@ -0,0 +1,14 @@
+package com.location.android.fridgefu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FridgeLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fridge_landing);
+    }
+}
