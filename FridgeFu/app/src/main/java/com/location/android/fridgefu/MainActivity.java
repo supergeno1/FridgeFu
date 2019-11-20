c7396d0da2f34b5e3d0997a16f4535448a0a701a
Renuka Nannapaneni <renuka@wirelessprv-10-194-248-17.near.illinois.edu>
2019-11-20 17:37:01 -0600

    premerge

---
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/MainActivity.java
@@ -0,0 +1,18 @@
+package com.location.android.fridgefu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(getBaseContext(), IndividualRecipe.class);
+        startActivity(intent);
+    }
+}
