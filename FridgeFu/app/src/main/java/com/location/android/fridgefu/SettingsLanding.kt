620a8117187260a9aeca6191c37483bfa2d71986
Renuka Nannapaneni <renuka@wirelessprv-10-194-248-17.near.illinois.edu>
2019-11-20 17:40:33 -0600

    merged and new branch

---
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/SettingsLanding.kt
@@ -0,0 +1,12 @@
+package com.location.android.fridgefu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsLanding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings_landing)
+    }
+}
