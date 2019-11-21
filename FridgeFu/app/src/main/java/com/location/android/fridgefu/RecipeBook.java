659399d8970492b602f92f32856a7f0e521b367b
varunsriram <varunsriram98@yahoo.com>
2019-11-20 20:50:13 -0600

    maybe fixed

---
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/RecipeBook.java
@@ -0,0 +1,15 @@
+package com.location.android.fridgefu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RecipeBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_book);
+    }
+
+}
