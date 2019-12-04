7b27f2c1ae0e78e93851f0d9c1fc14d58503aae1
Renuka Nannapaneni <renuka@Renukas-MBP.blank.com>
2019-12-04 05:25:09 -0600

    delete and moving between pages

--- FridgeFu/app/src/main/java/com/location/android/fridgefu/FridgeItem.java
+++ FridgeFu/app/src/main/java/com/location/android/fridgefu/FridgeItem.java
@@ -1,12 +1,37 @@
 package com.location.android.fridgefu;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
-public class FridgeItem {
+//public class FridgeItem {
+//    public String ingredient;
+//    public Calendar expiration_date;
+//    public boolean show_settings = false;
+//
+//    public FridgeItem (String ingredient, int year, int month, int day) {
+//        expiration_date = new GregorianCalendar();
+//        expiration_date.set(Calendar.YEAR, year);
+//        expiration_date.set(Calendar.MONTH, month);
+//        expiration_date.set(Calendar.DAY_OF_MONTH, day);
+//        this.ingredient = ingredient;
+//    }
+//
+////    @Override
+////    public String toString() {
+////        return "FridgeItem [ingredient=" + ingredient + ", expiration_date=" + expiration_date.toString() + ", show_settings=" + Boolean.toString(show_settings)
+////                + "]";
+////    }
+//}
+
+public class FridgeItem implements Parcelable {
     public String ingredient;
     public Calendar expiration_date;
     public boolean show_settings = false;
+
     public FridgeItem (String ingredient, int year, int month, int day) {
         expiration_date = new GregorianCalendar();
         expiration_date.set(Calendar.YEAR, year);
@@ -14,4 +39,41 @@ public class FridgeItem {
         expiration_date.set(Calendar.DAY_OF_MONTH, day);
         this.ingredient = ingredient;
     }
+
+//    @Override
+//    public String toString() {
+//        return "FridgeItem [ingredient=" + ingredient + ", expiration_date=" + expiration_date.toString() + ", show_settings=" + Boolean.toString(show_settings)
+//                + "]";
+//    }
+
+    protected FridgeItem(Parcel in) {
+        ingredient = in.readString();
+        expiration_date = (Calendar) in.readValue(Calendar.class.getClassLoader());
+        show_settings = in.readByte() != 0x00;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(ingredient);
+        dest.writeValue(expiration_date);
+        dest.writeByte((byte) (show_settings ? 0x01 : 0x00));
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<FridgeItem> CREATOR = new Parcelable.Creator<FridgeItem>() {
+        @Override
+        public FridgeItem createFromParcel(Parcel in) {
+            return new FridgeItem(in);
+        }
+
+        @Override
+        public FridgeItem[] newArray(int size) {
+            return new FridgeItem[size];
+        }
+    };
 }
